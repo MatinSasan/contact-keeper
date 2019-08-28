f4c0189174f1bef903ea12f530f9abefc895b0ec
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-28 15:28:03 +0430

    added user model and validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+const { check, validationResult } = require('express-validator');
+
+// validator
+
+const userValidator = [
+  check('name', 'Please add a name')
+    .not()
+    .isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 5 or more characters'
+  ).isLength({ min: 5 })
+];
+
 // @route POST api/users/
 // @desc Register a user
 // @access Public
 
-router.post('/', (req, res, next) => {
-  res.send('Register a user');
+router.post('/', userValidator, (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  res.send('passed');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect DB
+connectDB();
+
+// Middlewares
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res, next) => {
   res.json({ msg: 'testing this' });
