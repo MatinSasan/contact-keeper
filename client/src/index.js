2e26ff22f442399b72372d2befa531a4f52fade8
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-30 18:38:13 +0430

    auth context added

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/Authstate';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <ContactState>
-    <Router>
-      <App />
-    </Router>
-  </ContactState>,
+  <AuthState>
+    <ContactState>
+      <Router>
+        <App />
+      </Router>
+    </ContactState>
+  </AuthState>,
   document.getElementById('root')
 );
