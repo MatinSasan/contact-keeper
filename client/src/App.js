756d45421987597ef27049db9d8bd7e120aafdd4
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-30 19:38:55 +0430

    login and register components added and implemented

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Navbar from './components/layout/Navbar';
 const Home = lazy(() => import('./components/pages/Home'));
 const About = lazy(() => import('./components/pages/About'));
 const PageNotFound = lazy(() => import('./components/pages/PageNotFound'));
+const Register = lazy(() => import('./components/auth/Register'));
+const Login = lazy(() => import('./components/auth/Login'));
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
             <Route component={PageNotFound} />
           </Switch>
         </Suspense>
