369707c9e4d8ad6c21f2df123574028dde424bf7
Matin Sasanpour <matin.sasan@gmail.com>
2019-08-31 19:06:33 +0430

    PrivateRoute component added for home page

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Alerts from './components/layout/Alerts';
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 const Home = lazy(() => import('./components/pages/Home'));
 const About = lazy(() => import('./components/pages/About'));
 const PageNotFound = lazy(() => import('./components/pages/PageNotFound'));
 const Register = lazy(() => import('./components/auth/Register'));
 const Login = lazy(() => import('./components/auth/Login'));
-
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
 
 const App = () => {
   return (
@@ -25,7 +25,7 @@ const App = () => {
         <Alerts />
         <Suspense fallback={<h1>Please wait...</h1>}>
           <Switch>
-            <Route exact path='/' component={Home} />
+            <PrivateRoute exact path='/' component={Home} />
             <Route exact path='/about' component={About} />
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
